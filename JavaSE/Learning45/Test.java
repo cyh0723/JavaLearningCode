bc0ac50f9eb63c46ac8fb14b065931966466284c
YuhaoChen <642330125@qq.com>
2024-07-08 23:15:47 +0800

    idea提交

---
+++ JavaSE/Learning45/Test.java
@@ -0,0 +1,12 @@
+package JavaSE.Learning45;
+
+/**
+ * @author 陈宇浩
+ * @version 1.0
+ * Create by 2024/7/8 23:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
